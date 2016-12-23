3e26643da50c181b175fbddb3d0b3032619ccb1c
ginger1995 <jbw7@qq.com>
2016-12-24 00:10:00 +0800

    .

--- JavaCodes/src/com/jbw/algrothms/BinarySearch.java
+++ JavaCodes/src/com/jbw/algrothms/BinarySearch.java
@@ -17,7 +17,7 @@ public class BinarySearch {
 		}
 		return -1;
 	}
-
+	
 	public static void main(String[] args) {
 		int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 		System.out.println("Please enter a number:");
